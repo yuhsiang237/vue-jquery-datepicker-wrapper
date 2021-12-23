3a7c59bc03f7e1d4be6997c36636c4094a0460c1
Yu Hsiang <umworksite@gmail.com>
2021-12-23 20:25:12 +0800

    init

---
+++ DatePicker.js
@@ -0,0 +1,37 @@
+﻿Vue.component("date-picker", {
+    template: "<input/>",
+    props: ["dateFormat", "value"],
+    watch: {
+        value(value) {
+            $(this.$el).datepicker({
+                dateFormat: this.dateFormat,
+                onSelect: function (date) {
+                    self.$emit("input", date);
+                }
+            })
+            if (!(this.value == null || this.value == '')) {
+                $(this.$el).datepicker("setDate", new Date(this.value));
+            }
+        },
+        dateFormat(dateFormat) {
+            $(this.$el).datepicker("option", "dateFormat", dateFormat);
+        }
+    },
+    mounted: function () {
+        var self = this;
+        $(this.$el).datepicker({
+            dateFormat: this.dateFormat,
+            onSelect: function (date) {
+                self.$emit("input", date);
+            }
+        })
+        if (!(this.value === null || this.value === '')) {
+            $(this.$el).datepicker("setDate", new Date(this.value));
+        } 
+    },
+    beforeDestroy: function () {
+        $(this.$el)
+            .datepicker("hide")
+            .datepicker("destroy");
+    }
+});
